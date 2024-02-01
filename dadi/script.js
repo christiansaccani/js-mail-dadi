d7a006cc31c2adcae7de3f1825858bf67268673f
christiansaccani <christian.saccani99@gmail.com>
2024-02-01 12:54:01 +0100

    first commit

---
+++ dadi/script.js
@@ -0,0 +1,27 @@
+const userThrowBtn = document.querySelector("#UserThrow");
+let userResult;
+let computerResult;
+
+userThrowBtn.addEventListener("click",
+    function (event) {
+        event.preventDefault();   //To block auto-refresh
+        console.log("Invio Click");
+
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
+        userResult = getRandomInt(1, 6);
+        console.log("Risultato Utente:", userResult);
+
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
+        computerResult = getRandomInt(1, 6);
+        console.log("Risultato Computer:", computerResult);
+
+       
+
+
+
+    }
+)
